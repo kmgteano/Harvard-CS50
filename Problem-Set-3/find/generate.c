9b646dbab035f43ce160393a9b3093a40b42ac55
Katrina Teaño <kteano2006@my.fit.edu>
2015-05-01 09:10:00 -0400

    Imported generate.c with comments

---
+++ Problem-Set-3/find/generate.c
@@ -0,0 +1,55 @@
+/***************************************************************************
+ * generate.c
+ *
+ * Computer Science 50
+ * Problem Set 3
+ *
+ * Generates pseudorandom numbers in [0,LIMIT), one per line.
+ *
+ * Usage: generate n [s]
+ *
+ * where n is number of pseudorandom numbers to print
+ * and s is an optional seed
+ ***************************************************************************/
+ 
+// standard libraries
+#define _XOPEN_SOURCE
+#include <cs50.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+// constant
+#define LIMIT 65536
+
+int main(int argc, string argv[])
+{
+    // return error if given wrong # of arguments
+    if (argc != 2 && argc != 3)
+    {
+        printf("Usage: generate n [s]\n");
+        return 1;
+    }
+
+    // set number of pseudorandom numbers to make
+    int n = atoi(argv[1]);
+
+    // if seed is specified, set seed; else pick random.
+    if (argc == 3)
+    {
+        srand48((long int) atoi(argv[2]));
+    }
+    else
+    {
+        srand48((long int) time(NULL));
+    }
+
+    // print correct number of numbers given seed
+    for (int i = 0; i < n; i++)
+    {
+        printf("%i\n", (int) (drand48() * LIMIT));
+    }
+
+    // success
+    return 0;
+}
